5ee51e5d1f5892249a38249aeebc0397fa5730a5
hyejin <bonabita97@gmail.com>
2019-09-28 02:24:31 +0900

    testttt

--- sketch.js
+++ sketch.js
@@ -34,9 +34,9 @@ function preload() {
   rears[i] = loadImage('rears/rear' + i + '.png');
   lears[i] = loadImage('lears/lear' + i + '.png');
 
- 
-  
- 
+
+
+
   //nose = loadImage('n1.png');
   //reye = loadImage('re1.png');
   //leye = loadImage('le1.png');
@@ -58,7 +58,7 @@ function setup() {
   mic = new p5.AudioIn()
   mic.start();
   r = floor(random(0,6));
-  
+
   //video.size(width, height);
 
   // Create a new poseNet method with a single detection
@@ -97,7 +97,7 @@ function gotPoses(poses) {
   pose.nose.x = lerp(pose.nose.x, nX, 0.5);
   pose.nose.y = lerp(pose.nose.y, nY, 0.5);
   pose.nose.x = lerp(pose.nose.x, fX, 0.5);
-  pose.nose.y = lerp(pose.nose.y, fY, 0.5);  
+  pose.nose.y = lerp(pose.nose.y, fY, 0.5);
   pose.nose.x = lerp(pose.nose.x, hX, 0.5);
   pose.nose.y = lerp(pose.nose.y, hY, 0.5);
   pose.rightEye.x = lerp(pose.rightEye.x, reX, 0.5);
@@ -109,19 +109,19 @@ function gotPoses(poses) {
   //pose.nose.x = lerp(pose.nose.x, mX, 0.5);
   //pose.nose.y = lerp(pose.nose.y, mY, 0.5);
   pose.leftEar.x = lerp(pose.leftEar.x, leX, 0.5);
-  pose.leftEar.y = lerp(pose.leftEar.y, leY, 0.5);   
+  pose.leftEar.y = lerp(pose.leftEar.y, leY, 0.5);
   pose.rightEar.x = lerp(pose.rightEar.x, reX, 0.5);
-  pose.rightEar.y = lerp(pose.rightEar.y, reY, 0.5); 
+  pose.rightEar.y = lerp(pose.rightEar.y, reY, 0.5);
   //pose.rightWrist.x = lerp(pose.rightWrist.x, rwX, 0.5);
   //pose.rightWrist.y = lerp(pose.rightWrist.y, rwY, 0.5);
-  
-    
+
+
   }
 }
 
 function modelReady() {
   select('#status').html('Model Loaded');
-  
+
 }
 
 function draw() {
@@ -135,22 +135,22 @@ function draw() {
   pop();
   imageMode(CORNER);
   image(video, 0, 0);
-  if (vol > 0.1 ) {
+  if (mouseIsPressed) {
   r = floor(random(0,6));
   random(loadImage);
   } else {
   //image(faces[r]);
   //image(noses[r]);
-  
-  
-  
-  
+
+
+
+
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
-    
+
   let d = dist(pose.nose.x, pose.nose.y, pose.rightEye.x, pose.rightEye.y);
-		
+
     imageMode(CENTER);
     image(head, pose.nose.x, pose.nose.y - 120, d * 4, d * 3);
     image(faces[r], pose.nose.x, pose.nose.y, d * 3.5, d * 4);
@@ -166,8 +166,8 @@ function draw() {
     //image(mouth, pose.nose.x, pose.nose.y + 50, d * 1.25, d * 1.25);
     image(mouths[r], pose.nose.x, pose.nose.y + 50, d * 1.25, d * 1.25);
     //image(rwrist, pose.rightWrist.x, pose.rightWrist.y - 150, d * 4, d * 6 );
-    
-  
+
+
   }
 
   // We can call both functions to draw all keypoints and the skeletons
@@ -186,7 +186,7 @@ function drawKeypoints()  {
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
-		
+
     for (let j = 0; j < pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = pose.keypoints[j];
