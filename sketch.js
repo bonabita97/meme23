30e5e27db2e81e959926438494ceed2cefcf9f2e
hyejin <bonabita97@gmail.com>
2019-09-30 02:01:59 +0900

    fds

--- sketch.js
+++ sketch.js
@@ -15,7 +15,6 @@ var mouths = []; //입
 var reyes = []; //오른눈
 var leyes = []; //왼눈
 var noses = []; //코
-
 var rears = []; //오른귀
 var lears = []; //왼귀
 var rwrist = []; //오른 손목
@@ -57,51 +56,50 @@ function setup() {
 
 function gotPoses(poses) {
   if (poses.length > 0) {
+    let hX = poses[0].pose.keypoints[0].position.x //머리x =코 포지션
+    let hY = poses[0].pose.keypoints[0].position.y //머리y =코 포지션
+    let nX = poses[0].pose.keypoints[0].position.x //코x
+    let nY = poses[0].pose.keypoints[0].position.y //코y
+    let fX =  poses[0].pose.keypoints[0].position.x
+    let fY =  poses[0].pose.keypoints[0].position.x
+    let elX = poses[0].pose.keypoints[1].position.x //왼눈x
+    let elY = poses[0].pose.keypoints[1].position.y //왼눈y
+    let erX = poses[0].pose.keypoints[2].position.x //오른눈x
+    let erY = poses[0].pose.keypoints[2].position.y //오른눈y
+    let mX = poses[0].pose.keypoints[0].position.x //입x= 코 포지션
+    let mY = poses[0].pose.keypoints[0].position.y //입y =코 포지션
+    let reX = poses[0].pose.keypoints[4].position.x //오른귀x
+    let reY = poses[0].pose.keypoints[4].position.y //오른귀y
+    let rwX = poses[0].pose.keypoints[10].position.x //오른손목x
+    let rwY = poses[0].pose.keypoints[10].position.y //오른손목y
+    let lwX = poses[0].pose.keypoints[9].position.x //왼손목x
+    let lwY = poses[0].pose.keypoints[9].position.y //왼손목y
+    let leX = poses[0].pose.keypoints[3].position.x //왼귀x
+    let leY = poses[0].pose.keypoints[3].position.y //왼귀y
 
-
-  let hX = poses[0].pose.keypoints[0].position.x //머리x =코 포지션
-  let hY = poses[0].pose.keypoints[0].position.y //머리y =코 포지션
-  let nX = poses[0].pose.keypoints[0].position.x //코x
-  let nY = poses[0].pose.keypoints[0].position.y //코y
-  let elX = poses[0].pose.keypoints[1].position.x //왼눈x
-  let elY = poses[0].pose.keypoints[1].position.y //왼눈y
-  let erX = poses[0].pose.keypoints[2].position.x //오른눈x
-  let erY = poses[0].pose.keypoints[2].position.y //오른눈y
-  let mX = poses[0].pose.keypoints[0].position.x //입x= 코 포지션
-  let mY = poses[0].pose.keypoints[0].position.y //입y =코 포지션
-  let reX = poses[0].pose.keypoints[4].position.x //오른귀x
-  let reY = poses[0].pose.keypoints[4].position.y //오른귀y
-  let rwX = poses[0].pose.keypoints[10].position.x //오른손목x
-  let rwY = poses[0].pose.keypoints[10].position.y //오른손목y
-  let lwX = poses[0].pose.keypoints[9].position.x //왼손목x
-  let lwY = poses[0].pose.keypoints[9].position.y //왼손목y
-  let leX = poses[0].pose.keypoints[3].position.x //왼귀x
-  let leY = poses[0].pose.keypoints[3].position.y //왼귀y
-
-  pose.nose.x = lerp(pose.nose.x, nX, 0.5); //코
-  pose.nose.y = lerp(pose.nose.y, nY, 0.5); //코
-  pose.nose.x = lerp(pose.nose.x, fX, 0.5); //얼굴
-  pose.nose.y = lerp(pose.nose.y, fY, 0.5);  //얼굴
-  pose.nose.x = lerp(pose.nose.x, hX, 0.5); //머리
-  pose.nose.y = lerp(pose.nose.y, hY, 0.5); //머리
-  pose.rightEye.x = lerp(pose.rightEye.x, reX, 0.5); //오른눈
-  pose.rightEye.y = lerp(pose.rightEye.y, reY, 0.5); //오른눈
-  pose.leftEye.x = lerp(pose.leftEye.x, reX, 0.5); //왼눈
-  pose.leftEye.y = lerp(pose.leftEye.y, reY, 0.5); //왼눈
-  pose.nose.x = lerp(pose.nose.x, mX, 0.5); //입
-  pose.nose.y = lerp(pose.nose.y, mY, 0.5); //입
-  pose.leftEar.x = lerp(pose.leftEar.x, leX, 0.5); //왼귀
-  pose.leftEar.y = lerp(pose.leftEar.y, leY, 0.5); //왼귀
-  pose.rightEar.x = lerp(pose.rightEar.x, reX, 0.5); //오른귀
-  pose.rightEar.y = lerp(pose.rightEar.y, reY, 0.5); //오른귀
-  pose.rightWrist.x = lerp(pose.rightWrist.x, rwX, 0.5); //오른손
-  pose.rightWrist.y = lerp(pose.rightWrist.y, rwY, 0.5); //오른손
-  pose.leftWrist.x = lerp(pose.leftWrist.x, lwX, 0.5); //왼손
-  pose.leftWrist.y = lerp(pose.leftWrist.y, lwY, 0.5); //왼손
-
-
+    noseX = lerp(noseX, nX, 0.5); //코
+    noseY = lerp(noseY, nY, 0.5); //코
+    faceX = lerp(noseX, fX, 0.5); //얼굴
+    faceY= lerp(noseY, fY, 0.5);  //얼굴
+    headX = lerp(headX, hX, 0.5); //머리
+    headY = lerp(headY, hY, 0.5); //머리
+    rightEyeX = lerp(rightEyeX, reX, 0.5); //오른눈
+    rightEyeY = lerp(rightEyeY, reY, 0.5); //오른눈
+    leftEyeX = lerp(leftEyeX, reX, 0.5); //왼눈
+    leftEyeY = lerp(leftEyeY, reY, 0.5); //왼눈
+    mouthsX = lerp(mouthsX, mX, 0.5); //입
+    mouthsY = lerp(mouthsY, mY, 0.5); //입
+    leftEarX = lerp(leftEarX, leX, 0.5); //왼귀
+    leftEarY = lerp(leftEarY, leY, 0.5); //왼귀
+    rightEarX = lerp(rightEarX, reX, 0.5); //오른귀
+    rightEarY = lerp(rightEarY, reY, 0.5); //오른귀
+    rightWristX = lerp(rightWristX, rwX, 0.5); //오른손
+    rightWristY = lerp(rightWristY, rwY, 0.5); //오른손
+    leftWristX = lerp(leftWristX, lwX, 0.5); //왼손
+    leftWristY = lerp(leftWristY, lwY, 0.5); //왼손
   }
 }
+
 function modelReady() {
   console.log('model ready');
 }
@@ -131,27 +129,27 @@ function draw() {
     // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
 
-  let d = dist(pose.nose.x, pose.nose.y, pose.rightEye.x, pose.rightEye.y, pose.rightEar.x, pose.rightEar.y, pose.leftEar.x, pose.leftEar.y, pose.leftEye.x, pose.leftEye.y, pose.rightWrist.x, pose.rightWrist.y, pose.leftWrist.x, pose.leftWrist.y  );
+  let d = dist(noseX, noseY, leftEyeX, leftEyeY, rightEyeX, rightEyeY);
 
     imageMode(CENTER);
-    image(head, pose.nose.x, pose.nose.y - 120, d * 4, d * 3); //머리=코 y축 마이너스
+    image(head, noseX, noseY - 120 ); //머리=코 y축 마이너스
 
 
 
-    image(rears[r], pose.rightEar.x - 10, pose.rightEar.y, d * 1.25, d * 1.25); //오른귀
+    image(rears[r], rightEarX, rightEarY ); //오른귀
 
-		image(lears[r], pose.leftEar.x + 5, pose.leftEar.y - 10, d * 1.25, d * 1.25); //왼귀
-
-
-    image(noses[r], pose.nose.x, pose.nose.y - 15, d * 1.5, d * 1.5); //코
-
-    image(reyes[r], pose.rightEye.x, pose.rightEye.y, d * 1.75, d * 1.75); //오른눈
-
-    image(leyes[r], pose.leftEye.x, pose.leftEye.y, d * 1.5, d * 1.5); //왼눈
+		image(lears[r], leftEarX, leftEarY ); //왼귀
 
 
-    image(rwrist[r], pose.rightWrist.x, pose.rightWrist.y, d * 4, d * 6 ); //오른손
-    image(lwrist[r], pose.leftWrist.x, pose.leftWrist.y, d * 4, d * 6 ); //왼손
+    image(noses[r], noseX, noseY, d); //코
+
+    image(reyes[r], rightEyeX, rightEyeY, d); //오른눈
+
+    image(leyes[r], leftEyeX, leftEyeY, d); //왼눈
+
+
+    image(rwrist[r],rightWristX, rightWristY ); //오른손
+    image(lwrist[r], leftWristX, leftWristY ); //왼손
 
 
   }
