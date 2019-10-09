ee1f6a9e644ad895a8e4aae9a0a67782e6ab5e0e
hyejin <bonabita97@gmail.com>
2019-10-09 17:04:31 +0900

    fff444444

--- sketch.js
+++ sketch.js
@@ -1,212 +1,207 @@
-let ctracker; // face tracker
-let radio; // radio input
-let videoInput; // video input
-let xPosRectMask = 10; // rectangle boo x position
-let xStepRectMask = 100; // rectangle boo x step
-let button;
-function preload() {
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
 
-    // uploading images
-    imgcat = loadImage("assets/catmouth.png");
-    imgKing = loadImage("assets/king.png");
-    imgtear = loadImage("assets/tear.png");
-    imglefthand = loadImage("assets/lefthand.png");
-    imgrightehand = loadImage("assets/rightehand.png");
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+var mouths = [];
+var reyes = [];
+var leyes = [];
+var noses = [];
+var faces = [];
+var rears = [];
+var lears = [];
+var mic;
+var vol;
+let x = 0;
+let y = 0;
+
+let img;
+function preload() {
+  for (var  i = 0; i <2; i++) {
+  mouths[i] = loadImage('mouths/m' + i + '.png');
+  reyes[i] = loadImage('reyes/r' + i + '.png');
+  leyes[i] = loadImage('leyes/l' + i + '.png');
+  noses[i] = loadImage('noses/n' + i + '.png');
+  faces[i] = loadImage('faces/h' + i + '.png');
+  rears[i] = loadImage('rears/re' + i + '.png');
+  lears[i] = loadImage('lears/le' + i + '.png');
+
+
+
+
+  //nose = loadImage('n1.png');
+  //reye = loadImage('re1.png');
+  //leye = loadImage('le1.png');
+  //rear = loadImage('rear.png');
+  //lear = loadImage('lear3.png');
+  //face = loadImage('sky.png');
+  //mouth = loadImage('m2.png');
+  //head = loadImage('hair.png');
+  //rwrist = loadImage('lw.png');
+
+  }
 }
 
 function setup() {
+  createCanvas(640, 480);
+  //§createCanvas(windowWidth, windowHeight);
+  //video.size(width, height);
+  video = createCapture(VIDEO);
+  vidoe.size(640,480);
+  video.hide();
 
-    // setup camera capture
-    videoInput = createCapture();
-    videoInput.size(800, 600);
-    videoInput.position(200, 110);
-    videoInput.loop();
-    videoInput.hide();
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  // x = width/2
+  //y = height/2
+
+}
+function gotPoses(poses) {
+  if (poses.length > 0) {
+  let fX = poses[0].pose.keypoints[0].position.x
+  let fY = poses[0].pose.keypoints[0].position.y
+  let hX = poses[0].pose.keypoints[0].position.x
+  let hY = poses[0].pose.keypoints[0].position.y
+  let nX = poses[0].pose.keypoints[0].position.x
+  let nY = poses[0].pose.keypoints[0].position.y
+  let elX = poses[0].pose.keypoints[1].position.x
+  let elY = poses[0].pose.keypoints[1].position.y
+  let erX = poses[0].pose.keypoints[2].position.x
+  let erY = poses[0].pose.keypoints[2].position.y
+  let mX = poses[0].pose.keypoints[0].position.x
+  let mY = poses[0].pose.keypoints[0].position.y
+  let reX = poses[0].pose.keypoints[4].position.x
+  let reY = poses[0].pose.keypoints[4].position.y
+  //let rwX = poses[0].pose.keypoints[10].position.x
+  //let rwY = poses[0].pose.keypoints[10].position.y
+  let leX = poses[0].pose.keypoints[3].position.x
+  let leY = poses[0].pose.keypoints[3].position.y
+  pose.nose.x = lerp(pose.nose.x, nX, 0.5);
+  pose.nose.y = lerp(pose.nose.y, nY, 0.5);
+  pose.nose.x = lerp(pose.nose.x, fX, 0.5);
+  pose.nose.y = lerp(pose.nose.y, fY, 0.5);
+  pose.nose.x = lerp(pose.nose.x, hX, 0.5);
+  pose.nose.y = lerp(pose.nose.y, hY, 0.5);
+  pose.rightEye.x = lerp(pose.rightEye.x, reX, 0.5);
+  pose.rightEye.y = lerp(pose.rightEye.y, reY, 0.5);
+  pose.leftEye.x = lerp(pose.leftEye.x, reX, 0.5);
+  pose.leftEye.y = lerp(pose.leftEye.y, reY, 0.5);
+  pose.nose.x = lerp(pose.nose.x, mX, 0.5);
+  pose.nose.y = lerp(pose.nose.y, mY, 0.5);
+  //pose.nose.x = lerp(pose.nose.x, mX, 0.5);
+  //pose.nose.y = lerp(pose.nose.y, mY, 0.5);
+  pose.leftEar.x = lerp(pose.leftEar.x, leX, 0.5);
+  pose.leftEar.y = lerp(pose.leftEar.y, leY, 0.5);
+  pose.rightEar.x = lerp(pose.rightEar.x, reX, 0.5);
+  pose.rightEar.y = lerp(pose.rightEar.y, reY, 0.5);
+  //pose.rightWrist.x = lerp(pose.rightWrist.x, rwX, 0.5);
+  //pose.rightWrist.y = lerp(pose.rightWrist.y, rwY, 0.5);
 
 
-    // setup tracker
-    ctracker = new clm.tracker();
-    ctracker.init(pModel);
-    ctracker.start(videoInput.elt);
-
-    // create radio options
-    radio = createRadio();
-    radio.option("cool", 1);
-    radio.option("king", 2);
-    radio.option("soso", 3);
-    radio.option("what", 4);
-
-
-    // styling radio
-    radio.style('width', '60px');
-    radio.style('font-family', '"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif');
-    radio.style('line-height', '200%');
-    radio.position(1020, 120);
+  }
 }
 
+function modelReady() {
+  select('#status').html('Model Loaded');
+
+}
 
 function draw() {
-    var cnv = createCanvas(800, 600);
-    cnv.position(200, 110);
 
-    clear();
+  imageMode(CENTER);
+  image(video, 0, 0, width, height);
+
+  if (mouseIsPressed) {
+  r = floor(random(0,3));
+  random(loadImage);
+  }
+}
+
+
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+
+  let d = dist(pose.nose.x, pose.nose.y, pose.rightEye.x, pose.rightEye.y);
+
+    imageMode(CENTER);
+    image(faces[r], pose.nose.x, pose.nose.y - 120, d * 4, d * 3);
+    //image(faces[r], pose.nose.x, pose.nose.y, d * 3.5, d * 4);
+    //image(face, pose.nose.x, pose.nose.y, d * 3.5, d * 4);
+    //image(rear, pose.rightEar.x - 10, pose.rightEar.y, d * 1.25, d * 1.25);
+    image(rears[r], pose.rightEar.x - 10, pose.rightEar.y, d * 1.25, d * 1.25);
+    //image(lears[r], pose.leftEar.x + 5, pose.leftEar.y - 10, d * 1.25, d * 1.25);
+		image(lears[r], pose.leftEar.x + 5, pose.leftEar.y - 10, d * 1.25, d * 1.25);
+    //image(nose, pose.nose.x, pose.nose.y - 15, d * 1.5, d * 1.5);
+    image(noses[r], pose.nose.x, pose.nose.y - 15, d * 1.5, d * 1.5);
+    image(reyes[r], pose.rightEye.x, pose.rightEye.y, d * 1.75, d * 1.75);
+    image(leyes[r], pose.leftEye.x, pose.leftEye.y, d * 1.5, d * 1.5);
+    //image(mouth, pose.nose.x, pose.nose.y + 50, d * 1.25, d * 1.25);
+    image(mouths[r], pose.nose.x, pose.nose.y + 50, d * 1.25, d * 1.25);
+    //image(rwrist, pose.rightWrist.x, pose.rightWrist.y - 150, d * 4, d * 6 );
+
+
+  }
+
+  // We can call both functions to draw all keypoints and the skeletons
+  // drawKeypoints();
+  // drawSkeleton();
+}
+
+// A function to draw ellipses over the detected keypoints
+let logged = false;
+function drawKeypoints()  {
+	if (poses.length > 0 && !logged) {
+		console.log(poses);
+		logged = true;
+	}
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+		fill(255, 0, 255);
     noStroke();
-    var val = radio.value();
-
-    // draw each function based on radio selected
-    if (val == 1) {
-        drawFilterCool();
-    } if (val == 2) {
-        drawsadness();
-    } if (val == 3) {
-        drawMask();
-    } if (val == 4) {
-        drawking();
-    }
+		ellipse(pose.nose.x, pose.nose.y, 20, 20);
+  }
 }
 
-function drawFilterCool() {
-    drawGlasses();
-    drawHat();
-}
-
-function drawGlasses() {
-    // get array of face marker positions [x, y] format
-    var positions = ctracker.getCurrentPosition();
-    for (var i=0; i<positions.length; i++) {
-
-        // draw shapes based on marker positions
-        if (i == 27) {
-            fill(0);
-            ellipse(positions[i][0], positions[i][1], 80, 80);
-            rect(positions[i][0], positions[i][1], 3, 20);
-        }
-        if (i == 25) {
-            fill(0);
-            rect(positions[i][0], positions[i][1], 60, 10);
-        }
-        if (i == 32) {
-            fill(0);
-            ellipse(positions[i][0], positions[i][1], 80, 80);
-        }
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
     }
+  }
 }
-
-function drawHat() {
-    // get array of face marker positions [x, y] format
-    var positions = ctracker.getCurrentPosition();
-    for (var i=0; i<positions.length; i++) {
-
-        // draw shapes based on marker positions
-        if (i == 20) {
-            push();
-            translate(-70, -60);
-            fill(100, 50, 60);
-            rect(positions[i][0], positions[i][1], 280, 25);
-            pop();
-
-            push();
-            translate(-30, -140);
-            fill(100, 50, 60);
-            rect(positions[i][0], positions[i][1], 200, 100);
-            pop();
-        }
-    }
-}
-
-function drawcat() {
-    // draw transparent rectangle
-    fill(255, 255, 255, 50);
-    rect(0, 0, 800, 600);
-
-    // make video as pixels
-    videoInput.loadPixels();
-
-    // draw pixels
-    for (var cy = 0; cy < videoInput.height; cy += 1) {
-        for (var cx = 0; cx < videoInput.width; cx += 1) {
-            var offset = ((cy*videoInput.width)+cx)*4;
-            var xpos = (cx / videoInput.width) * width;
-            var ypos = (cy / videoInput.height) * height;
-            rect(xpos, ypos, 10, 10 * (videoInput.pixels[offset+1]/255));
-        }
-    }
-}
-
-function drawsadness() {
-    // draw transparent rectable on top
-    fill(0, 0, 0, 200);
-    rect(0, 0, 800, 600);
-
-    // draw strips
-    for (var i = 0; i < 10; i++) {
-        fill(0, 0, 0, 200);
-        rect(xPosRectMask+(xStepRectMask*i), 0, 40, 600);
-    }
-
-    // put image based on face tracker
-    var positions = ctracker.getCurrentPosition();
-    for (var i=0; i<positions.length; i++) {
-        if (i == 62) {
-            translate(-94, -160);
-            image(imgcat, positions[i][0], positions[i][1]);
-        }
-    }
-}
-
-//function drawking() {
-    // draw transparent rectangle
-  //  fill(53, 0, 81, 120);
-    //rect(0, 0, 800, 600);
-
-    // make video as pixels
-  //  videoInput.loadPixels();
-
-    // draw pixels
-    //var stepSize = round(constrain(20, 6, 32));
-        //for (var y=0; y<height; y+=stepSize) {
-        //for (var x=0; x<width; x+=stepSize) {
-          //var i = y * width + x;
-          //var darkness = (255 - videoInput.pixels[i*4]) / 255;
-          //var radius = stepSize * darkness;
-          //fill(random(255));
-          //ellipse(x, y, radius, radius);
-
-
-
-    // put image based on face tracker
-    var positions = ctracker.getCurrentPosition();
-    for (var i=0; i<positions.length; i++) {
-        if (i == 62) {
-            push();
-            translate(-150, -240);
-            image(imgKing, positions[i][0], positions[i][1]);
-            pop();
-        }
-
-}
-
-function drawsadness() {
-    // put image based on face tracker
-    var positions = ctracker.getCurrentPosition();
-    for (var i=0; i<positions.length; i++) {
-        if (i == 62) {
-            push();
-            translate(-57, -20);
-            image(imgcatmouth, positions[i][0], positions[i][1]);
-            pop();
-        }
-        if (i == 20) {
-            push();
-            translate(-100, -150);
-            image(imglefthand, positions[i][0], positions[i][1]);
-            pop();
-        }
-        if (i == 16) {
-            push();
-            translate(-20, -150);
-            image(imgrighthand, positions[i][0], positions[i][1]);
-            pop();
-        }
-    }
 }
